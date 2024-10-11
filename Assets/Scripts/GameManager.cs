c877a813ca90e674eedde6684245cb2bcb0c32c2
rachelle <93688951+rachelleliu@users.noreply.github.com>
2024-10-10 21:13:57 -0400

    Add start screen and high score display

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,42 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject startButton; //start button
     public Player player; //ref to player
-    public TMP_Text countdown; //countdown text
-    public TMP_Text scoreDisplay; //score display text
     public float count = 3f; //set countdown timer length
     public int score = 0; //set initial score
+    public int highScore = 0; //high score
+    private bool gameStarted = false; //boolean to track if game started
     
+    //UI elements
+    public GameObject startButton; //start button
+    public GameObject startMenu; //start menu
+    public GameObject overMenu; //game over menu
+    public TMP_Text highScoreDisplay; //high score display text
+    public TMP_Text countdown; //countdown text
+    public TMP_Text scoreDisplay; //score display text
     
     
     // Start is called before the first frame update
     void Start()
     {
-        //disable text displays and freeze game
+        //retrieve high score
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+
+        //disable unneeded UI and freeze game
         countdown.gameObject.SetActive(false);
         scoreDisplay.gameObject.SetActive(false);
+        overMenu.gameObject.SetActive(false);
         Time.timeScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //start game if user input detected and set gameStarted boolean to true
+        if(Input.GetButtonDown("Jump") && !gameStarted){
+            StartGame();
+        }
+        
         //display player score
         scoreDisplay.text = "Score: " + score.ToString();
         
@@ -47,8 +62,9 @@ public class GameManager : MonoBehaviour
     }
 
     public void StartGame(){
-        //disable start button
-        startButton.SetActive(false);
+        //disable start menu and set gameStarted bool to true;
+        startMenu.SetActive(false);
+        gameStarted = true;
         
         //unfreeze time
         Time.timeScale = 1;
@@ -60,6 +76,17 @@ public class GameManager : MonoBehaviour
     public void GameOver(){
         //freeze game
         Time.timeScale = 0;
+
+        //check if score is new high score and store new
+        if(score > highScore){
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        } 
+        
+        //display game over menu
+        overMenu.SetActive(true);
+        highScoreDisplay.text = "High Score: " + highScore.ToString();
     }
 
     public void Restart(){
